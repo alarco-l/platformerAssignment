27601e944ad6b5e6898d2d5edbf1daf81123b047
alarco-l <ludovic.alarcon@epitech.eu>
2015-11-14 01:23:14 +0900

    add cave

---
+++ PlatformerAssignment/Assets/Scripts/EntranceCave.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntranceCave : MonoBehaviour {
+	[SerializeField]
+	TerrainCollider col;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.tag == "Player") {
+			col.enabled = false;
+		}
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		if (collider.gameObject.tag == "Player") {
+			col.enabled = true;
+		}
+	}
+}
---
+++ PlatformerAssignment/Assets/Shaders/Depth.shader
@@ -0,0 +1,10 @@
+﻿Shader "Terrain Depth Mask Simple" {
+  SubShader {
+    Tags {"Queue" = "Geometry-200" } // earlier = hides stuff later in queue
+    Lighting Off
+    ZTest LEqual
+    ZWrite On
+    ColorMask 0
+    Pass {}
+  }
+}
